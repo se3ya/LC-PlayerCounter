d70ca8f8a12a39508a08117bc5c2598097eaa3d4
seeya <116365392+se3ya@users.noreply.github.com>
2025-11-08 13:12:49 +0200

    Info

--- src/PlayerCounter/PluginInfo.cs
+++ src/PlayerCounter/PluginInfo.cs
@@ -13,7 +13,7 @@ namespace PlayerCounter
     public static class MyPluginInfo
     {
         public const string PLUGIN_GUID = "com.seeya.mblcp.playercounter";
-        public const string PLUGIN_NAME = "MBLCP Player Tracker";
+        public const string PLUGIN_NAME = "Player Tracker";
         public const string PLUGIN_VERSION = "1.0.0";
     }
 }
